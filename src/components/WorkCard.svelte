21d3aa7e65603f317022e59b8da71b4fd8f9e684
mahefatsiresy <mahefatsiresy.n.lucas@gmail.com>
2026-01-28 14:41:51 +0300

    add a preview placeholder if not available, add preview for hay nahandro

--- src/components/WorkCard.svelte
+++ src/components/WorkCard.svelte
@@ -6,11 +6,19 @@
 <article
     class="relative rounded-lg border border-zinc-700/80 group overflow-hidden hover:-translate-y-1 duration-300 hover:border-green-500 hover:shadow-xl hover:shadow-green-500/40"
 >
-    <img
-        class="w-full aspect-video bg-zinc-800/80 rounded-lg object-cover rounded-b-none group-hover:scale-105 transition-transform duration-300"
-        src={project.image}
-        alt={`screenshot of ${project.name}`}
-    />
+    {#if project.image}
+        <img
+            class="w-full aspect-video bg-zinc-800/80 rounded-lg object-cover rounded-b-none group-hover:scale-105 transition-transform duration-300"
+            src={project.image}
+            alt={`screenshot of ${project.name}`}
+        />
+    {:else}
+        <div
+            class="w-full aspect-video bg-zinc-800/80 grid place-items-center text-zinc-400"
+        >
+            Preview unavailable
+        </div>
+    {/if}
     <div class="p-2">
         <h4 class="my-2 mb-0 font-semibold">
             <a href="/projects/{project.slug}"
