f3445f83099b98fdb4cf2756c25157b3f80a48e7
mahefatsiresy <mahefatsiresy.n.lucas@gmail.com>
2026-08-27 16:28:57 +0300

    update xp

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -3,6 +3,75 @@ import type { Experience } from "$lib/types";
 
 export const experiences: Experience[] = [
     {
+        role: "Full-Stack Developer & Tech Support",
+        date: "6-month internship at Zebutech (2026)",
+        tasks: [
+            "Develop extension mobile/desktop app for Zebutech's platform",
+            "Install Zebutech's softwares on client devices",
+            "Performed routine computer and software maintenance and troubleshooting.",
+            "Hosting websites for clients",
+        ],
+        works: [
+            {
+                slug: "gestion-nettoyage",
+                name: "Systeme de gestion de nettoyage",
+                date: "2026",
+                image: "",
+                description: "",
+                technologies: [
+                    "typescript",
+                    "Angular",
+                    "NestJS",
+                    "PostgreSQL",
+                    "Claude Code",
+                ],
+                skills: [],
+            },
+            {
+                slug: "kesykely-lite",
+                name: "KesyKely Lite",
+                date: "2026",
+                image: "",
+                description: "",
+                technologies: [
+                    "typescript",
+                    "React Native",
+                    "Angular",
+                    "SQLite",
+                    "Claude Code",
+                ],
+                skills: [],
+            },
+            {
+                slug: "kesykely-backup-utility",
+                name: "KesyKely Backup Utility",
+                date: "2026",
+                image: "",
+                description:
+                    "A java gui app to help user backup KesyKely database and automate backups.",
+                technologies: ["java"],
+                skills: [],
+            },
+            {
+                slug: "kesykely-data-analytics",
+                name: "KesyKely Data Analytics App",
+                date: "2026",
+                image: "",
+                description:
+                    "An app that help KesyKely users see how their business is performing. It works online so the user can access the app from anywhere.",
+                technologies: [
+                    "ETL",
+                    "typescript",
+                    "python",
+                    "Angular",
+                    "FastAPI",
+                    "Claude Code",
+                ],
+                skills: ["Create an ETL"],
+            },
+        ],
+    },
+    {
         role: "Full-Stack Developer Intern",
         date: "3-month internship at Enduma (2022)",
         tasks: [
